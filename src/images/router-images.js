6b0ea383f031ac5ffa7298f04328f78d858e191e
Lauraklijn <lauraklijn12@hotmail.com>
2020-03-09 10:32:35 +0100

    Created user-model

--- src/images/router-images.js
+++ src/images/router-images.js
@@ -3,12 +3,22 @@ const Image = require("../images/model-images");
 
 const router = new Router();
 
-router.get("/", (req, res, next) => {
-  Image.findAl()
-    .then(image => {
-      res.send(image);
+router.get("/images", (req, res, next) => {
+  Image.findAll()
+    .then(tickets => {
+      res.send(tickets);
     })
     .catch(next);
 });
 
+//create image
+router.post("/images", async (request, response, next) => {
+  console.log("create image...!!!!!!!!!!!!!!!!", request.body);
+  try {
+    const newImage = await Image.create(request.body);
+    response.send(newImage);
+  } catch (error) {
+    next(error);
+  }
+});
 module.exports = router;
---
+++ src/user/model-user.js
@@ -0,0 +1,24 @@
+const Sequelize = require("sequelize");
+const db = require("../../db");
+
+const User = db.define("user", {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  image: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+});
+
+module.exports = User;
